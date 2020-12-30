53c39b36fb76bf4f97b3b63e0c3fdd911138f96c
AC-xiaofan <1309549123@qq.com>
2020-12-30 10:07:54 +0800

    release

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径，记得修改哦
-const live2d_path = "https://cdn.jsdelivr.net/gh/Ac-xiaofan/888@1.0/live2d/";
-const tips_path = "https://cdn.jsdelivr.net/gh/Ac-xiaofan/888@1.0/live2d/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/Ac-xiaofan/111@1.0/live2d/";
+const tips_path = "https://cdn.jsdelivr.net/gh/Ac-xiaofan/111@1.0/live2d/";
 //const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
@@ -34,7 +34,7 @@ function loadExternalResource(url, type) {
 	]).then(() => {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
-			apiPath: "https://cdn.jsdelivr.net/gh/Ac-xiaofan/888@1.0/live2d_api/",
+			apiPath: "https://cdn.jsdelivr.net/gh/Ac-xiaofan/111@1.0/live2d_api/",
 			//cdnPath: "https://你的对象存储域名/live2d_api/"
 		});
 	});
